ec7cbfcad031f053e55c7fa9cdc9119d6d168649
Pablo Reyes Robles <pabloreyes500@gmail.com>
2021-02-05 13:08:46 -0300

    Modular substrate added

--- pureples/shared/substrate.py
+++ pureples/shared/substrate.py
@@ -1,8 +1,11 @@
 class Substrate(object):
 
-    def __init__(self, input_coordinates, output_coordinates, hidden_coordinates=(), res=10.0):
+    def __init__(self, input_coordinates, output_coordinates, hidden_coordinates=(), res=10.0, coordinates=()):
         self.input_coordinates = input_coordinates
         self.hidden_coordinates = hidden_coordinates        
         self.output_coordinates = output_coordinates
         self.res = res
+
+        # Modular substrate
+        self.coordinates = coordinates
 
